5478017d1cee849c22421d3bdf797af4939fc670
jfinch <jason.finch@gmail.com>
2016-01-28 17:19:12 +1000

    feat: (angular): factory to service  (and i suck at this scoping!)

--- src/SbManager/Scripts/manager/alertService.js
+++ src/SbManager/Scripts/manager/alertService.js
@@ -1,44 +1,33 @@
-﻿(function () {
+﻿(function() {
     'use strict';
-    angular.module('alerts',[])
-    .factory('alertService', alertService);
+    angular.module('alerts', [])
+        .service('alertService', alertService);
 
     function alertService() {
-        var service = {
-            add: add,
-            closeAlert: closeAlert,
-            closeAlertIdx: closeAlertIdx,
-            clear: clear,
-            get: get
-        },
-        alerts = [];
+        var that = this;
+        this.alerts = [];
 
-        return service;
-
-        function add(type, msg) {
-            return alerts.push({
+        this.add = function (type, msg) {
+            return this.alerts.push({
                 type: type,
                 msg: msg,
-                close: function () {
-                    return closeAlert(this);
+                close: function() {
+                    return that.closeAlert(this);
                 }
             });
-        }
+        };
 
-        function closeAlert(alert) {
-            return closeAlertIdx(alerts.indexOf(alert));
-        }
-
-        function closeAlertIdx(index) {
-            return alerts.splice(index, 1);
-        }
-
-        function clear() {
-            alerts = [];
-        }
-
-        function get() {
-            return alerts;
-        }
+        this.closeAlert = function (alert) {
+            return this.closeAlertIdx(this.alerts.indexOf(alert));
+        };
+        this.closeAlertIdx = function(index) {
+            return this.alerts.splice(index, 1);
+        };
+        this.clear = function() {
+            this.alerts = [];
+        };
+        this.get = function() {
+            return this.alerts;
+        };
     }
 })();
--- src/SbManager/Scripts/manager/pubSubService.js
+++ src/SbManager/Scripts/manager/pubSubService.js
@@ -1,31 +1,24 @@
-﻿//To subscribe:
-//PubSubService.subscribe(function() {
+﻿// To subscribe:
+// PubSubService.subscribe(function() {
 // event callback code
-//});
+// });
 
-//to emit
-//PubSubService.publish
-
-(function () {
+// to emit
+// PubSubService.publish
+(function() {
     'use strict';
-    angular.module('pubsub',[])
-    .factory('pubSubService', pubSubService);
+    angular.module('pubsub', [])
+        .service('pubSubService', pubSubService);
 
-    //http://becausejavascript.com/angularjs-pubsub-implementation-with-a-service/
     function pubSubService($rootScope) {
-        return {
-            subscribe: function (event,fn) {
-                //$rootScope.$on('notifying-service-event', function (e, data) {
-                $rootScope.$on(event, function (e, data) {
-                    fn(data);
-                });
-                //var handler = $rootScope.$on('notifying-service-event', callback);
-                //scope.$on('$destroy', handler);
-            },
-            publish: function(event,data) {
-                //$rootScope.$emit('notifying-service-event',data);
-                $rootScope.$emit(event, data);
-            }
-        }
-    }
-})();
+        this.subscribe = function(event, fn) {
+            $rootScope.$on(event, function(e, data) {
+                fn(data);
+            });
+        };
+        this.publish = function(event, data) {
+            $rootScope.$emit(event, data);
+        };
+    };
+})
+();
